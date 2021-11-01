fc7b2f0d6711517cf541110ea3a3d7fa11739d15
Mihail Sidov <mikegsid@gmail.com>
2021-11-01 03:47:36 +0200

    Add new problem solutions in Data Structures with C# Advanced

---
+++ 02. Data-Structures-Advanced/06. Hash-Tables Sets and Dictionaries-Exercise/01.RoyaleArena/Battlecard.cs
@@ -0,0 +1,59 @@
+namespace _01.RoyaleArena
+{
+    using System;
+
+    public class BattleCard : IComparable<BattleCard>
+    {
+        public BattleCard(int id, CardType type, string name, double damage, double swag) 
+        {
+            this.Id = id;
+            this.Type = type;
+            this.Name = name;
+            this.Damage = damage;
+            this.Swag = swag;
+        }
+
+        public int Id { get; set; }
+
+        public CardType Type { get; set; }
+
+        public string Name { get; set; }
+
+        public double Damage { get; set; }
+
+        public double Swag { get; set; }
+
+        public int CompareTo(BattleCard other) 
+        {
+            int compare = other.Damage.CompareTo(this.Damage);
+
+            if (compare == 0) 
+            {
+                compare = this.Id.CompareTo(other.Id);
+            }
+
+            return compare;
+        }
+
+        public override bool Equals(Object obj) 
+        {
+            if (this == obj) return true;
+
+            if (obj == null || this.GetType().Name.CompareTo(obj.GetType().Name) != 0) 
+                return false;
+            
+            BattleCard bc = (BattleCard)obj;
+            
+            return this.Id == bc.Id &&
+                   bc.Damage.Equals(this.Damage) &&
+                   bc.Swag.Equals(this.Swag) &&
+                   this.Type.Equals(bc.Type) &&
+                   this.Name.Equals(bc.Name);
+        }
+
+        public override int GetHashCode() 
+        {
+            return this.Id;
+        }
+    }
+}
---
+++ 02. Data-Structures-Advanced/06. Hash-Tables Sets and Dictionaries-Exercise/01.RoyaleArena/CardType.cs
@@ -0,0 +1,10 @@
+namespace _01.RoyaleArena
+{
+    public enum CardType 
+    {
+        MELEE,
+        RANGED,
+        SPELL,
+        BUILDING
+    }
+}
